f17529f2822844dddac64971859119b291e37eec
bahmanzadeh <rezabahmanzad@gmail.com>
2023-06-02 04:58:46 -0700

    add files

--- istio-ns-injection.sh
+++ istio-ns-injection.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 kubectl create namespace ns1 
 kubectl create namespace ns2
+kubectl create namespace ns3
 kubectl label namespace ns1 istio-injection=enabled
 kubectl label namespace ns2 istio-injection=enabled
+kubectl label namespace ns3 istio-injection=enabled
 #kubectl rollout restart deploy -n ns1
 #kubectl rollout restart deploy -n ns2
