edce91c1f9706947334a68e683b6e08055812416
bahmanzadeh <rezabahmanzad@gmail.com>
2023-10-02 05:38:08 -0700

    add new files

--- istio-install.sh
+++ istio-install.sh
@@ -2,9 +2,10 @@
 cd
 # curl -L https://istio.io/downloadIstio | sh -
 # copy istioctl to /usr/local/bin
-# sudo cp ~/istio-1.17.2/bin/istioctl /usr/local/bin/
+sudo cp ~/istio-1.19.0/bin/istioctl /usr/local/bin/
 # install
 istioctl install --set profile=demo -y
 # verify
+istioctl verify-install
 istioctl analyze
 istioctl version
